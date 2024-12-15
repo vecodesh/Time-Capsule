a1801f915e42a32fc6194f3ce6bf1daa67c50299
vecodesh <727723eucs268@skcet.ac.in>
2024-12-15 10:10:18 +0530

    Made the changes in app.js to resolve the errors and added some feautres

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
 import HomePage from "./components/HomePage";
-import "./App.css"; 
+import WriteLetter from "./components/WriteLetter";
+import "./App.css";
 
 const App = () => {
   return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/home" element={<HomePage />} />
-          </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/write-letter" element={<WriteLetter />} />
+      </Routes>
+    </Router>
   );
 };
 
