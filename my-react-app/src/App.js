d2cd0baf0dbe5ce40e9945dde2255f7eb774d139
vecodesh <727723eucs268@skcet.ac.in>
2024-12-15 08:19:01 +0530

    Update App.js with routing

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import Login from './LoginComponent/Login';
-import Signup from './LoginComponent/Signup';
-const App=()=>{
-  return(
-      <div>
-        <Login/>
-        <Signup/>
-      </div>
-  )
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import HomePage from "./components/HomePage";
+import "./App.css"; 
+
+const App = () => {
+  return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/home" element={<HomePage />} />
+          </Routes>
+      </Router>
+  );
+};
+
 export default App;
