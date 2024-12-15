c03eb7a687f319515a2973b6c4104be86f423cc6
vjsyam <127888696+vjsyam@users.noreply.github.com>
2024-12-15 16:45:04 +0530

    Updated LandingPage.js with a missing tag

--- my-react-app/src/components/LandingPage.js
+++ my-react-app/src/components/LandingPage.js
@@ -81,9 +81,11 @@ const LandingPage = () => {
             sx={{ color: "#444", maxWidth: "500px", margin: "0 auto" }}
           >
             Dear Future Me,
+            <br></br>
                 Hey there! What are you up to a year from now? Or, from your perspective, what was I doing a year ago? Time travel is weird. Honestly, I don’t know what to say—just a quick “Hi, how’s it going?” from your carefree, clueless past self.
                 If this feels silly, delete it—I won’t mind. But I hope it brings a smile and reminds you to enjoy life. Stay safe, have fun, and keep being awesome.
                 Cheers,
+                <br></br>
                 Your past self
           </Typography>
           <Box mt={3}>
